af93b085f710e04013a0cf31d4b29caad749b7e9
Tavis Weeden <tavisweeden@gmail.com>
2019-09-26 01:54:10 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -12,7 +12,7 @@ end
   introduction_with_language("Dan", "Ember.js")
 introduction_with_language("Josh", "React")
 
-def introduction_with_language_optional(name, language = Ruby)
+def introduction_with_language_optional(name, language = "Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
